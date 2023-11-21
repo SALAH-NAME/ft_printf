cc7b824563eaef47e191596c29707837c5456642
souahidi <souahidi@c3r8p3.1337.ma>
2023-11-21 21:48:16 +0100

    day 01

--- new_print.c
+++ new_print.c
@@ -1,8 +1,8 @@
-#include "ft_printf.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdarg.h>
 
 
 int	ft_putchar(const char c)
@@ -26,15 +26,22 @@ int	ft_putstr_null(const char *str)
 	return (count);
 }
 
-int	ft_print_base(size_t nb, const char *base, size_t len_base)
+int	ft_print_base(long nb, const char *base, size_t len_base, char flag)
 {
-	size_t	count;
-	size_t	n;
+	int     count;
+	long    n;
 
 	n = nb;
 	count = 0;
+	if (flag == 'p')
+	    count += ft_putstr_null("0x");
+	if (flag == '-' && n < 0)
+	{
+	    count += ft_putchar('-');
+	    n = -n;
+	}
 	if (n >= len_base)
-		count += ft_print_base(n / len_base, base, len_base);
+		count += ft_print_base(n / len_base, base, len_base, 0);
 	count += ft_putchar(base[n % len_base]);
 	return (count);
 }
@@ -48,29 +55,27 @@ int	ft_caller(const char *str, va_list ptr)
 	else if (*str == 'c')
 		return ft_putchar(va_arg(ptr, int));
 	else if (*str == 'd' || *str == 'i')
-		return ft_print_base(va_arg(ptr, int), "0123456789", 10);
+		return ft_print_base(va_arg(ptr, int), "0123456789", 10, '-');
 	else if (*str == 'p')
-		return ft_print_base(va_arg(ptr, unsigned int), "0123456789abcdef", 16);
+		return ft_print_base(va_arg(ptr, unsigned long), "0123456789abcdef", 16, 'p');
 	else if (*str == 'u')
-		return ft_print_base(va_arg(ptr, unsigned int), "0123456789", 10);
+		return ft_print_base(va_arg(ptr, unsigned int), "0123456789", 10, 0);
 	else if (*str == 'x')
-		return ft_print_base(va_arg(ptr, unsigned int), "0123456789abcdef", 16);
+		return ft_print_base(va_arg(ptr, unsigned int), "0123456789abcdef", 16, 0);
 	else if (*str == 'X')
-		return ft_print_base(va_arg(ptr, unsigned int), "0123456789ABCDEF", 16);
+		return ft_print_base(va_arg(ptr, unsigned int), "0123456789ABCDEF", 16, 0);
 	else if (*str == '%')
-		return ft_putchar(va_arg(ptr, int));
+		return ft_putchar('%');
 	else
-	{
-		ft_putchar('%');
-		ft_putchar(*str);
-		return (2);
-	}
+		return (-1);
+
 }
 
 int	ft_printf(const char *str, ...)
 {
 	int		count;
 	va_list	ptr;
+	int     checker;
 
 	count = 0;
 	va_start(ptr, str);
@@ -79,7 +84,10 @@ int	ft_printf(const char *str, ...)
 		if (*str == '%')
 		{
 			str++;
-			count += ft_caller(str, ptr);
+			checker = ft_caller(str, ptr);
+			if (checker == -1)
+			    return (-1);
+			count += checker;
 		}
 		else
 			count += ft_putchar(*str);
@@ -92,12 +100,16 @@ int	ft_printf(const char *str, ...)
 
 int main()
 {
-	int x = ft_printf("test int => %p", &x);
+	int x = ft_printf("test int => %%%>", &x);
 
 	printf("\nmy test = %d", x);
 	printf("\n\n");
 
-	x = printf("test int => %p", &x);
+	x = printf("test int => %%%\n", &x);
 	printf("\n   test = %d", x);
-	printf("\n\n");
+	
+// 	printf("\n\n+++ %d", printf("%>"));
+	
+
 }
+
